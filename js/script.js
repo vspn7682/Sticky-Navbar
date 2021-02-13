717301697ce68ee65f1951fcab6871e7b8ea3b95
Shailesh <vspn8569@gmail.com>
2021-02-13 13:09:19 +0530

    Responsiveness added

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ const navbar = document.querySelector('.navbar');
 // console.log(showcase);
 
 var height = showcase.clientHeight;
- console.log(height);
+//  console.log(height);
  
 window.addEventListener('scroll', () => {
-    if (document.body.scrollTop > height || document.documentElement.scrollTop > height) {
-        console.log(document.body.scrollTop)
-        console.log(height);
+    if (document.body.scrollTop > height - 100 || document.documentElement.scrollTop > height - 100) {
+        // console.log(document.body.scrollTop)
+        // console.log(height);
         
         navbar.classList.add('sticky');
     } else {
